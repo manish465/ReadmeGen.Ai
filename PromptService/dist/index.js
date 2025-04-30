903ccb9394243e97cfebfb52088fe4baa0a8f0a1
manish <ms5978307@gmail.com>
2025-04-30 15:00:48 +0530

    setup type script

---
+++ PromptService/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+const message = greet("TypeScript User");
+console.log(message);
+//# sourceMappingURL=index.js.map
--- PromptService/src/router/promptRouter.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { prompt, healthCheck } from "../service/promptService";
-
-const router = Router();
-
-router.post("/", prompt);
-router.post("/health-check", healthCheck);
