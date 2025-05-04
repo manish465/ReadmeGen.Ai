f81b98423d39f4f3578a82826d0b3c95239a8399
manish <ms5978307@gmail.com>
2025-05-04 16:32:36 +0530

    added dependency

--- PromptService/src/index.ts
+++ PromptService/src/index.ts
@@ -1,16 +1,5 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import promptRouters from './routers/promptRouters';
-
-const app: express.Application = express();
-dotenv.config();
-
-app.use(cors());
-app.use(express.json());
+import { app } from './app';
 
 const port: string = process.env.PORT || '3000';
 
-app.use("/api/v1/prompt", promptRouters);
-
-app.listen(port, () => console.log("Server is running in http://localhost:" + port));
+app.listen(port, () => console.log('Server is running in http://localhost:' + port));
