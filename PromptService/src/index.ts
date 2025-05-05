8ddecbaae9f47b548335e4c79c8fb73193c134ba
manish <46858945+manish465@users.noreply.github.com>
2025-05-05 13:53:29 +0530

    added consul register for prompt service (#4)

--- PromptService/src/index.ts
+++ PromptService/src/index.ts
@@ -1,5 +1,50 @@
+import Consul from 'consul';
 import { app } from './app';
 
 const port: string = process.env.PORT || '3000';
 
-app.listen(port, () => console.log('Server is running in http://localhost:' + port));
+const consulClient = new Consul({
+    host: 'consul-service',
+    port: 8500,
+    secure: false,
+});
+
+const registerService = async () => {
+    try {
+        await consulClient.agent.service.register({
+            id: process.env.NAME || 'default-service-id',
+            name: process.env.NAME || 'default-service-name',
+            address: 'localhost',
+            port: Number(port),
+            check: {
+                name: 'health-check',
+                http: `http://prompt-service:${port}/api/v1/prompt/health-check`,
+                interval: '10s',
+                timeout: '5s',
+                deregistercriticalserviceafter: '1m',
+            },
+        });
+        console.log('Service registered with Consul');
+    } catch (error) {
+        console.error('Failed to register service:', error);
+    }
+};
+
+const deregisterService = async () => {
+    try {
+        await consulClient.agent.service.deregister(process.env.NAME || 'default-service-id');
+        console.log('Service deregistered from Consul');
+    } catch (error) {
+        console.error('Failed to deregister service:', error);
+    }
+};
+
+app.listen(Number(port), async () => {
+    console.log('Server is running in http://localhost:' + port);
+    await registerService();
+});
+
+process.on('SIGINT', async () => {
+    await deregisterService();
+    process.exit();
+});
